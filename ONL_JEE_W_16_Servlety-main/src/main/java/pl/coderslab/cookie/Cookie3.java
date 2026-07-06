77300390536983c774297680870e4296b4f177b0
Jaro29 <jarofieducik@gmail.com>
2026-07-06 20:50:45 +0200

    Cookies cd

--- ONL_JEE_W_16_Servlety-main/src/main/java/pl/coderslab/cookie/Cookie3.java
+++ ONL_JEE_W_16_Servlety-main/src/main/java/pl/coderslab/cookie/Cookie3.java
@@ -9,23 +9,35 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/addToCookies")
-public class Cookie2 extends HttpServlet {
+@WebServlet("/cookie3")
+public class Cookie3 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String key = request.getParameter("key");
         String value = request.getParameter("value");
+        int time = getIntParameterOrDefault(request,"time",1);
         response.setContentType("text/html; charset=UTF-8");
-        if (key == null || key.isBlank() || value == null || value.isBlank()) {
+        if (key == null || key.isBlank() || value == null || value.isBlank() || time == 0) {
             response.getWriter().append("<h3>Wypełnij dane dla ciasteczka</h3>");
         } else {
             Cookie cookie = new Cookie(key, value);
-            cookie.setMaxAge(24 * 3600);
+            cookie.setMaxAge(time * 3600);
             cookie.setHttpOnly(true);
             cookie.setSecure(false);
             cookie.setAttribute("SameSite", "Lax");
             response.addCookie(cookie);
-            response.getWriter().append("<h3>Ciasteczko " + key + "zostało zapisane bezpiecznie!</h3>");
+            response.getWriter().append("<h3>Ciasteczko " + key + " zostało zapisane bezpiecznie!</h3>");
+        }
+    }
+    private int getIntParameterOrDefault(HttpServletRequest request, String paramName, int defaultValue) {
+        String value = request.getParameter(paramName);
+        if (value == null) {
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue; // Jeśli użytkownik wpisał tekst zamiast liczby, bezpiecznie zwracamy domyślną wartość!
         }
     }
 }
