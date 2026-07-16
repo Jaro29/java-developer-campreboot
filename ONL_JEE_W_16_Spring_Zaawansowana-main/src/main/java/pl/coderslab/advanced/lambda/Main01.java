a8f548224162b0cd4891dc7ef6d46fbf19d4b6fc
Jaro Gracz <jarofieducik@gmail.com>
2026-07-16 18:09:46 +0200

    Lambdy

--- ONL_JEE_W_16_Spring_Zaawansowana-main/src/main/java/pl/coderslab/advanced/lambda/Main01.java
+++ ONL_JEE_W_16_Spring_Zaawansowana-main/src/main/java/pl/coderslab/advanced/lambda/Main01.java
@@ -1,7 +1,25 @@
 package pl.coderslab.advanced.lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Main01 {
     public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            List<String> strings = new ArrayList<>();
+            System.out.println("Wpisuj dane, 'quit' kończy wprowadzanie:");
+            while (true) {
+                String input = scanner.nextLine().trim();
 
+                if (input.equalsIgnoreCase("quit")) {
+                    break;
+                }
+                strings.add(input);
+            }
+            strings.sort((a, b) -> a.compareTo(b));
+            System.out.println(strings);
+        }
     }
 }
